ecc236a879796f121291b8af879b882dcbc92a90
saylosmn <saylosmn@gmail.com>
2026-09-10 16:27:25 +0800

    Format sys.path.insert for better readability

--- api/index.py
+++ api/index.py
@@ -1,6 +1,9 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent / "backend"))
+sys.path.insert(
+    0,
+    str(Path(__file__).resolve().parent.parent / "backend")
+)
 
 from server import app
